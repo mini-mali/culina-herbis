bbc1fbb502f7288bd3e71f5c159c30d0eccc244c
frankie._.prankie <frantisek.rapcak@gmail.com>
2020-04-12 04:34:13 +0200

    > create app.js

---
+++ dist/app.js
@@ -0,0 +1,1 @@
+console.info('Hallow 🙏 World');
---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+console.log('Hallow 🙏 World');
+//console.warning('⚠ warning');
+//console.error('⧱ ERROR');
+//console.info('ℹ info');
+//❤️ console.table(tabularData, properties)
+//⭐ console.time("MyTimer 📖 https://console.spec.whatwg.org/#time ");
+//console.log('World 🙏 Hallow');
+
+
+
+
+//
+//  DOM ready
+//
+//  console.log('DOM ——— ready');
+//});
+
+
+
+
+//
+//  window RESIZE
+//
+//$(window).resize(function () {
+//  console.log('window ——— resized');
+//});
